5f9375afccf620fbf586b576a6f0b9b7bbfb6818
nameismohit <nameismohit001@gmail.com>
2022-07-27 21:10:50 +0530

    as

---
+++ pr1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>  
+//  int main()  
+// {  
+      
+//     int x=4;  
+//     if(x%2==0)  //this is a expression : expression is consists of operands and operators. 
+//     // There are 4 types of expressions  1.Arthematic(+-*/)   2.Relational(==,<=,>=,!=)   3.Logical(&&,||,!)   4.Conditional(exp1 ? exp2 : exp3)
+//     {  
+//         printf("The number x is even");  
+//     }  
+//     else  
+//     printf("The number x is not even");  
+//     return 0;  
+// }
+
+int main(){
+    int i,j;
+    for(i=0,j=0;i<10,j<10;i++);
+    {
+        printf("%d%d",i,j);
+    }
+    
+    return 0;
+
+}
